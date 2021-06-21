1354635d164257259a7077c3991cd0c988422c32
CreatorOfFame <40199794+CreatorOfFame@users.noreply.github.com>
2021-06-21 22:16:38 +0200

    h

--- The Grave Keeper/Assets/Scripts/DialogueHelper.cs
+++ The Grave Keeper/Assets/Scripts/DialogueHelper.cs
@@ -29,7 +29,7 @@ public class DialogueHelper : MonoBehaviour
         dialogueRunner.AddFunction("interactable",1,Interactable);
         dialogueRunner.AddFunction("scene",1,Scene);
         dialogueRunner.AddFunction("focus",1,ChangeFocus);
-        dialogueRunner.AddFunction("target",1,Target)
+        dialogueRunner.AddFunction("target",1,Target);
     }
     
     bool Visited(Yarn.Value[] parameters)
@@ -72,7 +72,7 @@ public class DialogueHelper : MonoBehaviour
         string focus = parameteres[0].AsString;
         string[] sections = focus.Split(',');
         Vector3 pos = new Vector3(float.Parse(sections[0]),float.Parse(sections[1]),float.Parse(sections[2]));
-        FindObjectOfType<DirectionPointerScript>().target = pos;
+        FindObjectOfType<DirectionPointerScript>().targetPosition = pos;
     }
 
     public void NodeComplete(string nodeName) {
