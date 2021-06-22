e50a11d37584761e5741664643fde10d09c15089
CreatorOfFame <40199794+CreatorOfFame@users.noreply.github.com>
2021-06-22 16:11:40 +0200

    playtestinsideALive

--- The Grave Keeper/Assets/Scripts/SmallScripts/FlickerScript.cs
+++ The Grave Keeper/Assets/Scripts/SmallScripts/FlickerScript.cs
@@ -5,18 +5,18 @@ using UnityEngine.Experimental.Rendering.Universal;
 
 public class FlickerScript : MonoBehaviour
 {
-    Light2D light;
+    Light2D source;
     public float off = 0;
     public float on = .8f;
     public string pattern = "mmamammmmammamamaaamammma";
     public float incrementInSeconds = .2f;
     IEnumerator Start()
     {
-        light = GetComponent<Light2D>();
+        source = GetComponent<Light2D>();
         while(true){
             foreach(char c in pattern){
                 float charnr = (char.ToUpper(c) - 'A')/26f;
-                light.intensity = (on - off) * charnr + off;//(on - off) * (/26) + off;
+                source.intensity = (on - off) * charnr + off;//(on - off) * (/26) + off;
                 yield return new WaitForSeconds(incrementInSeconds);
             }
         }
