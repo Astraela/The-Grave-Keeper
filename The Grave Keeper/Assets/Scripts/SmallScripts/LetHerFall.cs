8f5a2be657f73b0fc58e0189d677a117f8b3e88a
OppoOtis <joppedek2003@gmail.com>
2021-06-22 15:32:19 +0200

    deadbodyFinished

--- The Grave Keeper/Assets/Scripts/SmallScripts/LetHerFall.cs
+++ The Grave Keeper/Assets/Scripts/SmallScripts/LetHerFall.cs
@@ -2,13 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LetHerFall : MonoBehaviour
+public class LetHerFall : AnimationComponent
 {
     public GameObject fallPosition;
     public GameObject woman;
     public GameObject body;
 
-    public void SpawnWoman(){
+    public override void Animate(){
+        Debug.Log("AnimationRun");
         body = Instantiate(woman, fallPosition.transform.position, Quaternion.identity);
         body.transform.parent = gameObject.transform.GetChild(0).transform;
         body.transform.name = "DeadBody";
