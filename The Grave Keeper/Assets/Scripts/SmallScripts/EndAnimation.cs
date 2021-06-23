1f8b093b38358563745bf2b6ec4eea51e7971dd3
OppoOtis <joppedek2003@gmail.com>
2021-06-23 12:19:22 +0200

    laatstewatwemoetendoen

--- The Grave Keeper/Assets/Scripts/SmallScripts/EndAnimation.cs
+++ The Grave Keeper/Assets/Scripts/SmallScripts/EndAnimation.cs
@@ -11,13 +11,16 @@ public class EndAnimation : AnimationComponent
     public Light2D playerSpotlight;
     public Light2D wifeSpotlight;
     public Image fade;
+    public Sprite spriteObject;
     public override void Animate()
     {
         StartCoroutine("Lights");
+        Destroy(GetComponent<Animator>());
     }
 
     IEnumerator Lights(){
         while(true){
+            FindObjectOfType<PlayerMovement>().canMove = false;
             yield return new WaitForEndOfFrame();
             if(globalLight.intensity >= 0){
                 globalLight.intensity -= 0.002f;
@@ -25,6 +28,7 @@ public class EndAnimation : AnimationComponent
                 if(playerSpotlight.intensity <= 1f){
                     playerSpotlight.intensity += 0.001f;
                     wifeSpotlight.intensity += 0.001f;
+                    gameObject.GetComponent<SpriteRenderer>().sprite = spriteObject;
                 }else{
                     Color color = fade.color;
                     color.a += 1/255f;
