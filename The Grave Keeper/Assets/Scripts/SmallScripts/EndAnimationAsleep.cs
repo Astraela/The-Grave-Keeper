19ead5557b733433ed8439ad7b32988859a279a6
OppoOtis <joppedek2003@gmail.com>
2021-06-23 10:39:32 +0200

    insideScenes

---
+++ The Grave Keeper/Assets/Scripts/SmallScripts/EndAnimationAsleep.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class EndAnimationAsleep : AnimationComponent
+{
+    public Image fade;
+    public override void Animate()
+    {
+        StartCoroutine("Lights");
+    }
+
+    IEnumerator Lights(){
+        while(true){
+            yield return new WaitForEndOfFrame();
+            Color color = fade.color;
+            color.a += 1/255f;
+            fade.color = color;
+            //if(color.a >= 1){
+                //SceneManager.LoadScene(5);
+            //}
+        }
+    }
+}
